5c6a4e5a1155bb5c8d1af2ad2f210939dd3c88fa
Robert Churchill <robert@idacmedia.com>
2014-12-19 00:03:16 +0000

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Returns a list containing methods to get and set a (matrix) value
+## and to get and set a cached operation on that value.
+## Setting a new value will clear the cache.
+##
+## Args:
+##   x: A matrix that initialises the returned object with a value
+## Returns:
+##   A list with methods set, get, setcache and getcache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    get <- function() x
+    setcache <- function(i) cache <<- i
+    getcache <- function() cache
+    list(set=set, get=get,
+         setcache=setcache,
+         getcache=getcache)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of given matrix. Subsequent calls will return the
+## cached result. Assumes given matrix is solvable.
+##
+## Args:
+##   x: A matrix, represented by makeCacheMatrix list object
+## Returns:
+##   The inverse of x
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    i <- x$getcache()
+    if (is.null(i)) {
+        # cache is empty
+        i <- solve(x$get())
+        x$setcache(i)
+    }
+    i
 }
